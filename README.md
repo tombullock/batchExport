41e24bc2ed2c23479722ffcf0f538ca229b4e49c
tombullock <twbullock@googlemail.com>
2019-03-16 19:13:37 -0700

    updated folderwalk

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # batchExport
-Custom batch exporter for pupil labs
+""" 
+Authors: Py (adapted by TB to work with unix and legacy files) 
+Date: March 2019
 
-This program is run in python 3.x and needs packages like numpy and msgpack(version 0.5.6 needed!)
+Purpose: 
+Loop through recordings in a specified directory
+Update to latest version of recording format (if recorded in legacy format)
+Export data to .csv files 
+
+Notes: 
+Runs in python 3.x
+Needs packages like numpy, av etc.
+Needs msgpack 0.5.6 (latest version [6] breaks the export)
+Needs files from pupil/pupil_src/shared modules (git clone https://github.com/pupil-labs/pupil)
+IMPORTANT: I had to comment out line 26 in "update_methods.py" (from video_capture.utils import RenameSet) 
+to avoid having to install pyuvc (this caused problems when installing, and isn't needed for this script to work)
+
+To do: figure out how to export surface gaze data (currently only exports annotations and pupil data)
+
+'"""
--- folderwalk.py
+++ folderwalk.py
@@ -1,20 +1,49 @@
 """ 
-This file finds all the recordings the recording folder and extracts the data
+Authors: Py (adapted by TB to work with unix and legacy files) 
+Date: March 2019
+
+Purpose: 
+Loop through recordings in a specified directory
+Update to latest version of recording format (if recorded in legacy format)
+Export data to .csv files 
+
+Notes: 
+Runs in python 3.x
+Needs packages like numpy, av etc.
+Needs msgpack 0.5.6 (latest version [6] breaks the export)
+Needs files from pupil/pupil_src/shared modules (git clone https://github.com/pupil-labs/pupil)
+IMPORTANT: I had to comment out line 26 in "update_methods.py" (from video_capture.utils import RenameSet) 
+to avoid having to install pyuvc (this caused problems when installing, and isn't needed for this script to work)
+
+To do: figure out how to export surface gaze data (currently only exports annotations and pupil data)
+
 '"""
+
+# set paths
+pupilSourcePath = "/Users/tombullock/Documents/Psychology/BOSS/pupil/pupil_src/shared_modules" # location of pupil src files
+recording_path = ['/Users/tombullock/Documents/Psychology/BOSS/PUPIL_TEST_EXPORT'] # recordings you want to export
+out_dir = '/Users/tombullock/Documents/Psychology/BOSS/PUPIL_TEST_EXPORTED' # exported .csv files destination folder
+
+# export annotations
+annotations = True
+
 
 import os
 import extract_diameter
-
-recording_path = ['C:/User/recordings']
+import sys
+sys.path.insert(0,pupilSourcePath)
+import update_methods
 
 recording_valid = []
-
 csv_out = []
-out_dir = 'C:/User/ExportedRecordings'
-annotations = False
 
 for pa in recording_path:
     recording_dates = os.listdir(pa)
+    
+    # edit for unix compatibility
+    if '.DS_Store' in recording_dates:
+        recording_dates.remove('.DS_Store')
+    
     for da in recording_dates:
         folder = os.path.join(pa, da)
     
@@ -24,14 +53,17 @@ for pa in recording_path:
         for se in recording_sessions:
             # Find all recordings for each day
             session = os.path.join(folder, se)
+            
+            #add update methods here with session
+            update_recording_to_recent(session)
 
             if os.path.isdir(session):
                 recording_valid.append(session)
                 csv_out.append('pupil_positions_'+ da + '_' + se + '.csv')
 
                 if annotations:
-                    csv_out.append('annotations_' + da + '_' + se + '.csv') 
-            
+                    csv_out.append('annotations_' + da + '_' + se + '.csv')
+
 extract_diameter.main(recording_valid, csv_out, out_dir, overwrite = True, annotations = annotations)
 
 
